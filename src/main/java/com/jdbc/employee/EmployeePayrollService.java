1f32a4031ad0bde15af779145bdee0f60b012072
Dinesh130914 <dineshthiyagu99@gmail.com>
2022-01-29 17:24:45 +0530

    Find sum number of male,female in Payroll_service Db

--- src/main/java/com/jdbc/employee/EmployeePayrollService.java
+++ src/main/java/com/jdbc/employee/EmployeePayrollService.java
@@ -1,6 +1,7 @@
 package com.jdbc.employee;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,15 +13,20 @@ import com.mode.EmpPayrollData;
 
 public class EmployeePayrollService {
 	SqlQuries sql = new SqlQuries();
+	List<EmpPayrollData> emplist = new ArrayList<>();
 
 	public static void main(String[] args) {
 		Connection con = DatabaseConnector.getconnection();
 		EmployeePayrollService empservice = new EmployeePayrollService();
-		
+		empservice.countEmployees(con);
+		empservice.getTotalSalary(con);
+		// empservice.getEmpList(con);
+		// empservice.getEmpDataonDateWithParticularRange("2016-02-14-", "2021-01-27",
+		// con);
 	}
 
 	public List<EmpPayrollData> getEmpList(Connection con) {
-		List<EmpPayrollData> emplist = new ArrayList<>();
+
 		try {
 			Statement selectstatement = con.createStatement();
 			ResultSet rs = selectstatement.executeQuery(sql.GET_EMPLOYEE_QUERY);
@@ -36,7 +42,7 @@ public class EmployeePayrollService {
 			emplist.forEach(emp -> {
 				System.out.println("Emp Id : " + emp.getEmp_id());
 				System.out.println("Emp Name : " + emp.getName());
-				System.out.println("Salary :"+emp.getSalary());
+				System.out.println("Salary :" + emp.getSalary());
 				System.out.println("Gender : " + emp.getGender());
 				System.out.println("Date of Joining : " + emp.getStart());
 				System.out.println("----------------------------------------------------");
@@ -49,14 +55,14 @@ public class EmployeePayrollService {
 
 	}
 
-	public int updatesalary( double salary,String name, Connection con) {
-		int sts=0;
+	public int updatesalary(double salary, String name, Connection con) {
+		int sts = 0;
 		try {
 			PreparedStatement ps = con.prepareStatement(sql.UPDATE_SALARY);
-			
+
 			ps.setDouble(1, salary);
 			ps.setString(2, name);
-			sts=ps.executeUpdate();
+			sts = ps.executeUpdate();
 			ps.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -64,4 +70,62 @@ public class EmployeePayrollService {
 		return sts;
 	}
 
+	public List<EmpPayrollData> getEmpDataonDateWithParticularRange(String start, String end, Connection con) {
+		try {
+			EmpPayrollData rangeData = new EmpPayrollData();
+			PreparedStatement ps = con.prepareStatement(sql.DATE_RANGE_EMP_DATA);
+			ResultSet dateRs = ps.executeQuery();
+			while (dateRs.next()) {
+				rangeData.setEmp_id(dateRs.getInt("id"));
+				rangeData.setName(dateRs.getString("name"));
+				rangeData.setGender(dateRs.getString("gender"));
+				rangeData.setStart(dateRs.getDate("start_date"));
+				rangeData.setSalary(dateRs.getDouble("salary"));
+			}
+			emplist.forEach(emp -> {
+				System.out.println("Emp_Id" + emp.getEmp_id());
+				System.out.println("Name" + emp.getName());
+				System.out.println("Gender" + emp.getGender());
+				System.out.println("Salary" + emp.getSalary());
+				System.out.println("Start_Date" + emp.getStart());
+			});
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return emplist;
+
+	}
+
+	public void countEmployees(Connection con) {
+		int maleEmpCount = 0, femaleEmpCount = 0;
+		try {
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("select gender,count(*) from employee_payroll group by gender");
+			while (rs.next()) {
+				if (rs.getString("gender").equals("M")) {
+					maleEmpCount = rs.getInt("count(*)");
+				} else {
+					femaleEmpCount = rs.getInt("count(*)");
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Male employees are : " + maleEmpCount);
+		System.out.println("Female employees are : " + femaleEmpCount);
+	}
+
+	public void getTotalSalary(Connection con) {
+		long totalSalary = 0;
+		try {
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("select sum(basic_pay) from employee_payroll");
+			while (rs.next()) {
+				totalSalary = rs.getLong(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Total salary paid by organisation : " + totalSalary);
+	}
 }
--- src/main/java/com/jdbc/employee/SqlQuries.java
+++ src/main/java/com/jdbc/employee/SqlQuries.java
@@ -3,4 +3,5 @@ package com.jdbc.employee;
 public class SqlQuries {
 	public String GET_EMPLOYEE_QUERY = "select*from employee_payroll";
 	public String UPDATE_SALARY="update employee_payroll set salary=? where name=?;";
+	public String DATE_RANGE_EMP_DATA="select * from employee_payroll where start_date>='?' and start_date<='?';";
 }
